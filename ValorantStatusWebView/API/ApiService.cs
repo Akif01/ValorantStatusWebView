d7f6f42fb76166e58f65054d6abd937a5c0cc5c3
Akif Öztürk <66181296+Akif01@users.noreply.github.com>
2025-03-15 20:05:23 +0700

    Moved url to api service and changed tests  correspondingly

--- ValorantStatusWebView/API/ApiService.cs
+++ ValorantStatusWebView/API/ApiService.cs
@@ -1,21 +1,39 @@
-﻿namespace ValorantStatusWebView.API
+﻿using ValorantStatusWebView.DataTransferObjects;
+using ValorantStatusWebView.Models;
+using static ValorantStatusWebView.Components.Shared.RegionStatusCard;
+
+namespace ValorantStatusWebView.API
 {
     public class ApiService
     {
         private readonly HttpClient _httpClient;
+        private readonly ConfigurationService _configService;
 
-        public ApiService(HttpClient httpClient)
+        public ApiService(HttpClient httpClient, ConfigurationService configService)
         {
             _httpClient = httpClient;
+            _configService = configService;
         }
 
-        public async Task<TDto> GetAsync<TDto>(string url)
-            where TDto: class
+        private string GetBaseUrl(Regions region)
+        {
+            return $"https://{region}.api.riotgames.com/val/status/v1/platform-data?api_key={_configService.ApiKey}";
+        }
+
+        public async Task<PlatformModel?> GetPlatformModelAsync(Regions region)
+        {
+            var url = GetBaseUrl(region);
+            var response = await GetAsync<PlatformDataDto>(url);
+
+            return response is not null ? new PlatformModel(response) : null;
+        }
+
+        public async Task<TDto?> GetAsync<TDto>(string url)
+            where TDto : class
         {
             var response = await _httpClient.GetAsync(url);
             response.EnsureSuccessStatusCode();
-            var dto = await response.Content.ReadFromJsonAsync<TDto>();
-            return dto;
+            return await response.Content.ReadFromJsonAsync<TDto>();
         }
     }
 }
