8f3e9d9cd218f29da9139237087c0e1a3c2c22bd
Akif Öztürk <66181296+Akif01@users.noreply.github.com>
2025-04-04 23:44:10 +0700

    Made apiService handle errors gracefully

--- ValorantStatusWebView/API/IApiService.cs
+++ ValorantStatusWebView/API/IApiService.cs
@@ -1,10 +1,13 @@
 ﻿using static ValorantStatusWebView.Components.Shared.RegionStatusCard;
 using ValorantStatusWebView.Models;
+using System.Net.Http.Headers;
+using System.Text.Json;
 
 namespace ValorantStatusWebView.API
 {
     public interface IApiService
     {
+        Task<TDto?> GetAsync<TDto>(string url, CancellationToken cancellationToken = default, HttpHeaders? headers = null, JsonSerializerOptions? jsonOptions = null) where TDto : class;
         Task<PlatformModel?> GetPlatformModelAsync(Regions region, CancellationToken cancellationToken = default);
     }
 }
